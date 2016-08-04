96cb4a38e83b2233b5bc0fc22f91d49235b60711
Maggie Tan <maggietanzhenzhen@gmail.com>
2016-08-04 15:01:00 -0400

    update

--- BoxApp.js
+++ BoxApp.js
@@ -15,10 +15,55 @@ BoxApp.UI = function () {
     console.log('assembling UI ...');
     var div = document.getElementById("boxDiv");
     BoxApp.div = div;
-    $('<h4>Loading data from Box</h4>').appendTo(div);
-
-    $('<div id="box-select" data-link-type="direct"  data-client-id="cowmrbwt1f8v3c9n2ucsc951wmhxasrb"></div>').appendTo(div);
+    $('<h4>Loading data from Box.com，Local Drive and Dropbox</h4>').appendTo(div);
+    //box
+    $('<div id="box-select" data-link-type="direct"  data-client-id="einxjwtgxkwjqaf7pa7xsqyux7znoym2"></div>').appendTo(div);
     $('<div id="loadingBox" style="color:red"> loading Box.com ... </div>').appendTo(div);
+    //localFile
+    $('<div><input type="file" id="localFile" value="Local File"></div>').appendTo(div);
+    //dropbox
+    $('<div id="dropBox-select"></div>').appendTo(div);
+    $('<div id="loadingDropbox" style="color:red"> loading DropBox.com ... </div>').appendTo(div);
+
+
+
+    // load file
+      localFile.onchange = function () {
+          var f = this.files[0];
+          var reader = new FileReader();
+          reader.onload = function (x) {
+              BoxApp.loadFile(x.target.result, f.name)
+          };
+          reader.readAsText(f)
+      };
+
+      // load DropBox
+
+          var s = document.createElement('script');
+          s.src = "https://www.dropbox.com/static/api/2/dropins.js";
+          s.id = "dropboxjs";
+          s.type = "text/javascript";
+          s.setAttribute("data-app-key", "4wdr30ewnj2ut7q");
+          s.onload = function () {
+              console.log('loaded dropbox file picker');
+              var button = Dropbox.createChooseButton(options);
+              document.getElementById("dropBox-select").appendChild(button);
+              $('#loadingDropbox').remove()
+          };
+          document.body.appendChild(s);
+
+          options = {
+              success: function (files) {
+                  //console.log("Files", files)
+                  var url = files[0].link;
+                  BoxApp.loadDropbox(url)
+              },
+              cancel: function () {
+              },
+              linkType: "direct",
+              multiselect: false
+              //extensions: ['.json', '.txt', '.csv'],
+          };
 
     // load Box.com
     $.getScript("https://app.box.com/js/static/select.js").then(function () {
@@ -45,11 +90,21 @@ BoxApp.loadBox = function (x) {
 
 treemap(url)
 
-        }
-       // div.innerHTML=x
+        };
+BoxApp.loadDropbox = function (x) {
+        console.log('loading data from Dropbox.com ...');
+        var url = x[0].url;
 
-        //BoxApp.fun(x,url)
+    treemap(url)
 
+            };
+      BoxApp.loadFile = function (x) {
+        console.log('loading data from local File...');
+        var url = x[0].url;
+
+    treemap(url)
+
+            };
 
 BoxApp.fun = function (x, url) {
     BoxApp.log(x.length + ' entries sampled from ' + url, 'blue');
